a22c471f757622905f2232bc4f1a1cb55d8c9fb7
spartazhc <spartazhc@gmail.com>
2020-01-10 20:26:54 +0800

    av1 over rtp, only have split mode

--- libavformat/rtpenc_av1.c
+++ libavformat/rtpenc_av1.c
@@ -9,7 +9,7 @@ static uint8_t firstPacketReceived = 0;
 #define AGGRE_HEADER_SIZE 1
 #define AV1_RTP_FLAG_Z 0x80
 #define AV1_RTP_FLAG_Y 0x40
-#define AV1_RTP_FLAG_N 0x04
+#define AV1_RTP_FLAG_N 0x08
 #define AV1_RTP_FLAG_W1 0x10
 
 #define AV1_RTP_FLAG_NONE 0
@@ -75,61 +75,39 @@ static void flush_buffered(AVFormatContext *s1, int last)
     s->buffered_nals = 0;
 }
 
+// 只把一个obu拆成多个rtp_payload，没有合并多个obu到一个rtp_payload
 static void obu_send(AVFormatContext *s1, const uint8_t *buf, int size, int last)
 {
     RTPMuxContext *s = s1->priv_data;
 
-    uint8_t *obu_ele_hdr;
+    uint8_t *obu_ele_hdr, first;
     size_t obu_ele_siz;
     int header_size;
     obu_ele_hdr = (uint8_t *)malloc(sizeof(obu_ele_hdr) * 2);
     // if (size <= 0)
     //     return AVERROR_INVALIDDATA;
     eb_aom_uleb_encode(size, sizeof(size), obu_ele_hdr, &obu_ele_siz);
-
+    header_size = obu_ele_siz + AGGRE_HEADER_SIZE;
 
     av_log(s1, AV_LOG_DEBUG, "Sending OBU Type: %x of len %d M=%d\n", buf[0] & 0x7F, size, last);
 
-    int buffered_size = (int)(s->buf_ptr - s->buf);
-    header_size = buffered_size > 0 ? obu_ele_siz : (obu_ele_siz + AGGRE_HEADER_SIZE);
-
-    // 直接可以放到前一个包里
-    if (buffered_size + size + header_size <= s->max_payload_size) {
-        if (buffered_size == 0) {
-            s->buf_ptr++;
-        }
-        update_aggregate_hdr(s1, AV1_RTP_FLAG_NONE, 0);//?
-        memcpy(s->buf_ptr, obu_ele_hdr, obu_ele_siz);
-        memcpy(s->buf_ptr + obu_ele_siz, buf, size);
-        s->buf_ptr += (size + obu_ele_siz);
-        // s->buffered_nals++;
-        // will flush in next obu_send or at end of code
+    if (size + header_size <= s->max_payload_size) {
+        update_aggregate_hdr(s1, AV1_RTP_FLAG_NONE, 0);
+        memcpy(s->buf + AGGRE_HEADER_SIZE, obu_ele_hdr, obu_ele_siz);
+        memcpy(s->buf + header_size, buf, size);
+        ff_rtp_send_data(s1, buf, size + header_size, last);
     } else {
         av_log(s1, AV_LOG_DEBUG, "OBU size %d > %d\n", size, s->max_payload_size);
-        // 可以先把前一个塞满
-        if (buffered_size > 0) {
-            int size_to_fill = s->max_payload_size - buffered_size - AGGRE_HEADER_SIZE;
-
-            if (size_to_fill > 127) {
-                size_to_fill -= 2;
-            } else {
-                size_to_fill--;
-            }
-            eb_aom_uleb_encode(size_to_fill, sizeof(size_to_fill), obu_ele_hdr, &obu_ele_siz);
-
-            update_aggregate_hdr(s1, AV1_RTP_FLAG_Y, 0);
-
-            memcpy(s->buf_ptr, obu_ele_hdr, obu_ele_siz);
-            memcpy(s->buf_ptr + obu_ele_siz, buf, size_to_fill);
-            buf  += size_to_fill;
-            size -= size_to_fill;
-            flush_buffered(s1, 0);
-        }
-
-        // 再处理剩下的部分
+        first = 1;
         while (size + AGGRE_HEADER_SIZE > s->max_payload_size) {
-            update_aggregate_hdr(s1, AV1_RTP_FLAG_Z | AV1_RTP_FLAG_Y | AV1_RTP_FLAG_W1, 1);
-            memcpy(&s->buf[AGGRE_HEADER_SIZE], buf, s->max_payload_size - AGGRE_HEADER_SIZE);
+            // first rtp_payload should not have AV1_RTP_FLAG_Z
+            if (first == 1) {
+                update_aggregate_hdr(s1, AV1_RTP_FLAG_Y | AV1_RTP_FLAG_W1, 1);
+                first = 0;
+            } else {
+                update_aggregate_hdr(s1, AV1_RTP_FLAG_Z, 0);
+            }
+            memcpy(s->buf + AGGRE_HEADER_SIZE, buf, s->max_payload_size - AGGRE_HEADER_SIZE);
             ff_rtp_send_data(s1, s->buf, s->max_payload_size, 0);
             buf  += s->max_payload_size - AGGRE_HEADER_SIZE;
             size -= s->max_payload_size - AGGRE_HEADER_SIZE;
@@ -146,14 +124,13 @@ static void obu_send(AVFormatContext *s1, const uint8_t *buf, int size, int last
 
 void ff_rtp_send_av1(AVFormatContext *s1, const uint8_t *buf, int size)
 {
-    // const uint8_t *r, *end = buf + size;
     RTPMuxContext *s = s1->priv_data;
+    int64_t obu_size;
+    int start_pos, type, temporal_id, spatial_id;
+
 
     s->timestamp = s->cur_timestamp;
     s->buf_ptr   = s->buf;
-
-    int64_t obu_size;
-    int start_pos, type, temporal_id, spatial_id;
 
     if (!firstPacketReceived) {
         update_aggregate_hdr(s1, AV1_RTP_FLAG_N, 1);
